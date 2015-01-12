63912ea21ec572a95e84bf6fb91e448ab26d9eb5
Joel <joelg@mit.edu>
2015-01-12 00:22:23 -0500

    Bugfixes in preflop.py

--- preflop.py
+++ preflop.py
@@ -2,8 +2,13 @@
 import itertools
 
 def getEq(hand):
-    if str(type(hand)) == "<class 'str'>":
+    
+    if str(type(hand)) == "<type 'str'>":
         hand = [hand[:2], hand[2:]]
+    
+    hand[0] = hand[0][0].upper() + hand[0][1]
+    hand[1] = hand[1][0].upper() + hand[1][1]
+
     if hand[0][1] == hand[1][1]:
         # Same suit
         hand = hand[0][0] + hand[1][0] + "s"
@@ -13,7 +18,6 @@ def getEq(hand):
     else:
         # Different suit and number
         hand = hand[0][0] + hand[1][0] + "o"
-        
     with open("preflop.csv") as data:
         s = data.read()
         if s.find(hand) == -1:
@@ -22,5 +26,5 @@ def getEq(hand):
         # Find hand in file
         eq = s[s.find(hand):s.find("\n", s.find(hand) + 1)]
         eq = float(eq[eq.find(",") + 1:])
-        print(eq)
+        return eq
 
