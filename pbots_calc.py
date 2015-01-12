31d8fee9afe8c63e73399f2c887a38219e93e7cf
Jeffrey Hu <hujh14@episcopalacademy.org>
2015-01-11 23:28:38 -0500

    equity calc working on mac

--- pbots_calc.py
+++ pbots_calc.py
@@ -28,14 +28,14 @@ import ctypes
 import ctypes.util
 import sys
 
-'''if sys.platform.startswith('win'):
+if sys.platform.startswith('win'):
     pbots_calc = "pbots_calc"
 elif sys.platform.startswith('darwin'):
     pbots_calc = "libpbots_calc.dylib"
 else:
     pbots_calc = "libpbots_calc.so"
-'''
-pbots_calc = "pbots_calc"
+
+#pbots_calc = "pbots_calc"
 
 class _Results(ctypes.Structure):
     _fields_ = [("ev", ctypes.POINTER(ctypes.c_double)),
@@ -94,6 +94,8 @@ def calc(hands, board = '', dead = '', iters = 1000000):
         results = None
     pcalc.free_results(res)
     return results
+
+print calc([['Ah','Ad'],['Kh','Kd']])
 
 # 'results' is a Results object that includes ev
 # (the probability distribution, in list form),
