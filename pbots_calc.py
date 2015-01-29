76382415a736b24a9b806f9845a954315a4fdf19
Joel <joelg@mit.edu>
2015-01-28 20:03:19 -0500

    Made pbots_calc compatible with Python3.4
    Because this is totally our greatest problem right now

--- pbots_calc.py
+++ pbots_calc.py
@@ -46,7 +46,7 @@ class _Results(ctypes.Structure):
 try:
     pcalc = ctypes.CDLL(pbots_calc)
 except OSError:
-    print "ERROR: Could not locate %s. Please ensure your enviroment library load path is set properly." % pbots_calc
+    print("ERROR: Could not locate %s. Please ensure your enviroment library load path is set properly.") % pbots_calc
     sys.exit(1)
 
 # Set the argtype and return types from the library.
@@ -90,7 +90,7 @@ def calc(hands, board = '', dead = '', iters = 1000000):
     if err > 0:
         results = Results(res[0])
     else:
-        print "error: could not parse input or something..."
+        print("error: could not parse input or something...")
         results = None
     pcalc.free_results(res)
     return results
